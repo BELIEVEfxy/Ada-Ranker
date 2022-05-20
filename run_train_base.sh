63b4ca54908111a10080fc1d8da292028e5f0422
BELIEVEfxy <xinyanruc@126.com>
2022-05-20 10:07:10 +0800

    update

--- run_train_base.sh
+++ run_train_base.sh
@@ -9,7 +9,7 @@ MY_DIR="/home/xinyan_fan/SIGIR2022/Ada-Ranker/"
 ALL_DATA_ROOT="/home/xinyan_fan/SIGIR2022/dataset/processed_data_test/"
 
 # overall config
-MODEL_NAME='NARM' # [MF, GRU4Rec, SASRec, NARM, NextItNet, SRGNN, SHAN]
+MODEL_NAME='GRU4Rec' # [MF, GRU4Rec, SASRec, NARM, NextItNet, SRGNN, SHAN]
 DATASET_NAME="ML10M" # [Taobao, Xbox, ML10M]
 train_type='Base' # ['Base', 'Ada-Ranker']
 
